4c2905b1fe7d1e9d7ed0862a1fccd64136810a7a
Alumno_T <Alumno_T@PC-03>
2021-04-22 18:03:51 +0200

    Creación de la clase de prueba llamada CalculadoraTest por Alejandro Ruiz Sánchez

---
+++ test/arscalculadora/calculadoraTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arscalculadora;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ *
+ * @author Alumno_T
+ */
+public class calculadoraTest {
+    
+    public calculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
